10f6e6ec9fc75bd183ede067a5139c1677570fcb
Gregory Hellings <greg.hellings@gmail.com>
2021-03-02 23:25:42 -0600

    Fix lint problems

--- src/sword_tools_python/bin/modcompare.py
+++ src/sword_tools_python/bin/modcompare.py
@@ -1,38 +1,48 @@
-from .. import utils
 import argparse
 import sys
-import Sword
+
+import Sword  # pylint: disable=import-error
+
+from .. import utils
 
 
-def modcompare(arglist=sys.argv):
-    parser = argparse.ArgumentParser(description="Compares two SWORD modules, verse by"
-            " verse, to certify that the contents of each are identical")
+def modcompare(arglist=sys.argv):  # pylint: disable=dangerous-default-value
+    """
+    Compares two modules, verse by verse, and reports verses that differ between
+    the two.
+    """
+    parser = argparse.ArgumentParser(
+        description="Compares two SWORD modules, verse by"
+        " verse, to certify that the contents of each are identical"
+    )
     parser.add_argument("original", help="First module")
     parser.add_argument("test", help="Second module to compare against the first")
-    args = parser.parse_args()
+    args = parser.parse_args(arglist)
 
     original = utils.get_mod(args.original)
     test = utils.get_mod(args.test)
 
     if original is None or test is None:
-        print("Please double check that both modules are installed and accessible",
-              file=sys.stderr)
+        print(
+            "Please double check that both modules are installed and accessible",
+            file=sys.stderr,
+        )
         sys.exit(1)
 
     key = Sword.VerseKey(original.createKey())
     test.setKey(key)
     book = key.getBook()
 
-    while key.popError() == '\x00':
+    while key.popError() == "\x00":
         if book != key.getBook():
             print("Checked up through ", key.getText())
             book = key.getBook()
-        oText = original.renderText().c_str()
-        tText = test.renderText().c_str()
-        if oText != tText:
+        original_text = original.renderText().c_str()
+        test_text = test.renderText().c_str()
+        if original_text != test_text:
             print(f"Mismatch in {key.getText()}")
-            print(f"Original text is*********:\n{oText}")
-            print(f"New text is*********:\n{tText}")
+            print(f"Original text is*********:\n{original_text}")
+            print(f"New text is*********:\n{test_text}")
         key.increment()
         original.setKey(key)
         test.setKey(key)
